79a81a1d93c4a02c4ab94c5322adffd64e94e935
cser-srishti-beaspal <cser.srishti.beaspal@gmail.com>
2026-03-30 00:09:48 +0530

    added Test Cases

--- middleware-service-layer-testing/src/test/java/com/capgemini/ScientificCalculatorTest.java
+++ middleware-service-layer-testing/src/test/java/com/capgemini/ScientificCalculatorTest.java
@@ -1,16 +1,20 @@
 package com.capgemini;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ScientificCalculatorTest {
 	@Test
-	
-	public void mulTest() {
+	public void mulTestCase1() {
 		int actual = ScientificCalculator.multiplication(5,5);
 		int expected = 25;
+		Assert.assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void mulTestCase2() {
+		int actual = ScientificCalculator.multiplication(5,5);
+		int expected = -25;
 		Assert.assertEquals(actual, expected);
 	}
 }
